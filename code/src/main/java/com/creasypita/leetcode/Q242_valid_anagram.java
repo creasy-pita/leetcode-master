4c604f1b7a93670363bee3013fa234bbc3a285bf
creasy-pita <hz_ljq@live.com>
2022-11-09 13:41:17 +0800

    leetcode NO242异位词

---
+++ code/src/main/java/com/creasypita/leetcode/Q242_valid_anagram.java
@@ -0,0 +1,31 @@
+package com.creasypita.leetcode;
+
+/**
+ * Created by lujq on 11/9/2022.
+ */
+public class Q242_valid_anagram {
+    public boolean isAnagram(String s, String t) {
+        int[] record = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            record[s.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            record[t.charAt(i) - 'a']--;
+        }
+
+        for (int count: record) {
+            if (count != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s = "aee";
+        String t = "eae";
+        System.out.println("t is a valid anagram for s:" + new Q242_valid_anagram().isAnagram(s, t));
+    }
+}
--- code/src/main/java/com/creasypita/leetcode/Q704_binary_search.java
+++ code/src/main/java/com/creasypita/leetcode/Q704_binary_search.java
@@ -2,8 +2,9 @@ package com.creasypita.leetcode;
 
 /**
  * Created by lujq on 11/8/2022.
+ * tag :数组
  */
-public class Q1 {
+public class Q704_binary_search {
 
     public int search(int[] nums, int target) {
         // 避免当 target 小于nums[0] nums[nums.length - 1]时多次循环运算
@@ -26,7 +27,7 @@ public class Q1 {
     public static void main(String[] args) {
 //        int[] a = new int[]{ 1,4,6,9,100,105 };
         int[] intArray = new int[]{  1,4,6,9,100,105 };
-        System.out.println("search result :" + new Q1().search(intArray, 6));
+        System.out.println("search result :" + new Q704_binary_search().search(intArray, 6));
 
     }
 }
